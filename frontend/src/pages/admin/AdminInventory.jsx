1f16225d33abcd249d1ba94d145abb1c223c1e45
Sgt.Oddball <s.arnado.536447@umindanao.edu.ph>
2025-04-12 18:59:25 +0800

    frontend

--- frontend/src/pages/admin/AdminInventory.jsx
+++ frontend/src/pages/admin/AdminInventory.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const AdminInventory = () => {
   return (
-    <div className="px-6 py-8 ">
-      <div className="text-6xl font-bold">Inventory</div>
+    <div className="px-15 py-5">
+      <div className="bg-[#BFC8E5] w-[1540px] h-[880px] flex flex-col justify-start gap-y-5 overflow-y-auto pt-4 rounded-xl">
+        <div className="px-6 py-0 ">
+          <div className="text-6xl font-bold">Inventory</div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AdminInventory
+export default AdminInventory;
--- frontend/src/pages/admin/AdminTransactionHistory.jsx
+++ frontend/src/pages/admin/AdminTransactionHistory.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const AdminTransactionHistory = () => {
   return (
-    <div>AdminTransactionHistory</div>
+    <div className="px-15 py-5">
+      <div className="bg-[#BFC8E5] w-[1540px] h-[880px] flex flex-col justify-start gap-y-5 overflow-y-auto pt-4 rounded-xl">
+        <div className="px-6 py-0 ">
+          <div className="text-6xl font-bold">Transaction History</div>
+        </div>
+      </div>
+    </div>
   )
 }
 
